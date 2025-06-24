5a853163568ceed0a33e4a0640bfc36c058a3831
TransformologyXR <84306552+TransformologyXR@users.noreply.github.com>
2025-06-24 16:06:25 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,8 @@
-DATABASE_URL = 'postgresql://user:pass@host:port/dbname'
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql://txr:Secret123%40@txr-postgres.postgres.database.azure.com:5432/aqi"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autoflush=False)
+Base = declarative_base()
