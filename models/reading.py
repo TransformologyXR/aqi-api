27b5ca56b6f7eb0eac5faebf0fe62f4c3cffa1e6
TransformologyXR <84306552+TransformologyXR@users.noreply.github.com>
2025-06-24 16:46:37 +0100

    Update reading.py

--- models/reading.py
+++ models/reading.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, Numeric, String, DateTime, ForeignKey
-from app.database import Base
+from database import Base
 
 class Reading(Base):
     __tablename__ = "aqi_readings"
